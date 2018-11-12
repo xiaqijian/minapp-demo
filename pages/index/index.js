ed8239211d2443100db69a1a03c44279fc0ef03b
xiaqijian <2817062995@qq.com>
2018-11-12 08:52:44 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onClickDisabled(event) {
+    console.log(event)
+  }
+  
+  
+})
