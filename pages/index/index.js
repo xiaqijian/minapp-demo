0d4ef65c3c8d300d696251a872e27dc1926e295f
xiaqijian <2817062995@qq.com>
2018-11-12 22:13:51 +0800

    添加首页数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,27 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    aLists:[]
   },
   onClickDisabled(event) {
     console.log(event)
+  },
+  onReady() {
+    console.log(999)
+    this.getData()
+  },
+  getData () {
+    const that= this
+    wx.request({
+      url: 'https://easy-mock.com/mock/5be7d39ab953a11dfd8ce5b5/example/index', //仅为示例，并非真实的接口地址
+      success(res) {
+        console.log(res.data.data.list)
+        let arr = res.data.data.list
+        that.setData({
+          aLists: arr
+        })
+      }
+    })
   }
   
   
